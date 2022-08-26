b77bd92b8ef3962c441513a79b075457339a6b48
@mukul.kansal <mukul.kansal@jungleworks.com>
2022-08-26 14:52:23 +0530

    automation

---
+++ jenkins-integration.js
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fmodern\fcharset0 Courier-Oblique;\f1\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red10\green9\blue26;\red255\green255\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c3922\c3922\c13725;\cssrgb\c100000\c100000\c100000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\i\fs35\fsmilli17600 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 node \{
+\f1\i0\fs44 \cb3 \
+
+\f0\i\fs35\fsmilli17600 \cb1 sh \'91echo HelloWorld\'92
+\f1\i0\fs44 \cb3 \
+
+\f0\i\fs35\fsmilli17600 \cb1 \}}
