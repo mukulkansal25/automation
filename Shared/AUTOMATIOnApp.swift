eb1d837bb267ed709f15ea8ac6fbf900f3bb629d
@mukul.kansal <mukul.kansal@jungleworks.com>
2022-08-26 14:45:17 +0530

    Initial Commit

---
+++ Shared/AUTOMATIOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUTOMATIOnApp.swift
+//  Shared
+//
+//  Created by Mukul Kansal on 26/08/22.
+//
+
+import SwiftUI
+
+@main
+struct AUTOMATIOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
